e643e9946ebd26550a5841f9b469907b8de2063f
CrgioPeca88 <crgiopeca88@gmail.com>
2020-10-05 12:06:05 -0500

    [FEATURE] defaultProps feature checked - upload-index.js adjusted

--- src/components/upload/index.js
+++ src/components/upload/index.js
@@ -5,6 +5,21 @@ import React, {Component} from 'react';
 import './upload.css';
 import UploadInput from '../commons/upload_input';
 
+function FunctionComponent(props) {
+  const { titulo, color } = props;
+  return (
+    <div className="test-section">
+      <h2>{ titulo }, component from function!</h2>
+      <span style={{color: color}}>Property = { titulo }</span>
+    </div>
+  );
+}
+
+FunctionComponent.defaultProps = {
+  color: 'red',
+  titulo: 'Test default props'
+};
+
 class Upload extends Component {
   render() {
     return (
@@ -12,6 +27,8 @@ class Upload extends Component {
           <h1>Upload file!</h1>
           <span>You can upload the <strong>ubigeos.txt</strong> file located in the project data folder</span>
           <UploadInput/>
+          <FunctionComponent titulo="Test" color="gray"></FunctionComponent>
+          <FunctionComponent></FunctionComponent>
         </div>
     );
   }
